00f9d5fa739bd7dd0eac6d48adf2bfbe14a55b38
Dasari Srikanth <91716647+38110120@users.noreply.github.com>
2022-07-21 12:53:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let name="mosh";
+console.log(name);
+let age=30;
+console.log(age);
+
+let personDetails = {name: 'john', number:'60'};
+console.log(personDetails);
+personDetails.name = 'dany';
+console.log(personDetails);
+personDetails.number = '80';
+console.log(personDetails);
+
+
+let isapproved = 'true';
+let isnotapproved = 'false';
+console.log(true);
+console.log(isnotapproved);
+
+let selectedcolours = {first: 'red', second: 'yellow', third:'green'};
+console.log(selectedcolours);
+let number = 70;
+console.log(number);
+typeof number;
