2b6526af6b38d2dd85df27a0600310df04df2707
Dasari Srikanth <91716647+38110120@users.noreply.github.com>
2022-07-21 12:50:03 +0530

    Add files via upload

---
+++ function add().js
@@ -0,0 +1,39 @@
+function add(num1 = 10, num2 = 20)
+{
+    return num1 + num2;
+
+}
+let result =  add();
+console.log(result);
+
+
+function division(num1 , num2)
+{
+    return num1 % num2;
+}
+let output = division(19, 4)
+console.log(output); 
+
+
+function square(number)
+{
+    return number * number;
+}
+let value = square(3);
+console.log(value);
+
+function table(number)
+{
+    for(let i=0;i<=20;i++){
+        let res = i*number;
+        console.log(res);
+    }
+}
+table(5);
+
+
+
+
+         
+
+    
